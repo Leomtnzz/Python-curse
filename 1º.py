f267c7b829d4a9ddcca6ea0c18d23eafd5aaaa73
Leomtnzz <leomwetzels13@gmail.com>
2024-06-25 10:24:59 +0200

    Add files via upload
    The things i'm learning in my python course.

---
+++ 1º.py
@@ -0,0 +1,72 @@
+first='Leo'
+last='Martínez'
+
+#pizza=str('pepperoni')
+#print(type(pizza))
+#print(type(pizza)==str)
+#print(isinstance(pizza, str))
+
+
+
+#concatenación
+#fullname= first+' '+last
+#print(fullname)
+#fullname+= '?'
+#print(fullname)
+
+
+
+#casting a number to a string
+#decade=str(1980)
+#print(type(decade))
+#print(decade)
+
+#afirmación='me gusta la música de los ' + decade+'s.'
+#print(afirmación)
+
+
+
+#varias líneas
+
+multilínea='''
+Hola, cómo estás?                               
+
+Pasaba para saludar.
+                        Todo bien?
+
+'''
+#print(multilínea)
+
+
+
+#escapar caracteres especiales.
+
+#frase='i\'m back at work!\t hey!\n\n Where\'s this at\\located?'
+#print(frase)
+
+
+
+#métodos string
+
+#print(first)
+#print(first.lower())
+#print(first.upper())
+#print(first)
+
+#print(multilínea.title())
+#print(multilínea.replace('bien', 'ok'))
+#print(multilínea)
+
+#print(len(multilínea))
+#multilínea+= '                                        '
+#multilínea='               '+multilínea
+#print(len(multilínea))
+
+#print(len(multilínea.strip()))
+#print(len(multilínea.lstrip()))
+#print(len(multilínea.rstrip()))
+
+
+
+#construir un menú
+
