dba84264586cfc0ea2b10882b591c2d3ed87a95c
Leomtnzz <leomwetzels13@gmail.com>
2024-07-14 18:16:50 +0200

    Add files via upload

---
+++ Curso de Python/8º lección/loops.py
@@ -0,0 +1,48 @@
+#WHILE LOOP
+value=1
+#while value<=10:
+    #print(value)
+    #if value==5:
+        #break
+    #value+=1
+    #print(value)
+    
+#while value<=10:
+ #   value+=1
+  #  if value==5:
+   #     continue
+    #print(value)
+#else:
+ #   print('Value is now equal to '+ str ( value))
+
+
+#FOR LOOPS
+
+names=['Dave','Sara','John']
+#for x in names:
+    #print(x)
+    
+#for x in 'Mississippi':
+    #print(x)
+    
+    
+#for x in names:
+    #if x=='Sara':
+     #   break
+    #print(x)
+    
+#for x in names:
+ #   if x=='Sara':
+  #      continue
+   # print(x)
+   
+#for x in range(4):
+#    print(x)
+
+#for x in range(2,4):
+#    print(x)
+
+for x in range(-10,101,5):
+    print(x)
+else:
+    print('Glad that\'s over!')
