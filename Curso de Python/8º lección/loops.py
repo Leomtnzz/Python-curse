dcd95081dfcc16070c24097cc80426485539ff00
Leomtnzz <leomwetzels13@gmail.com>
2024-07-15 16:22:12 +0200

    Add files via upload

---
+++ Curso de Python/8º lección/loops.py
@@ -0,0 +1,68 @@
+#WHILE LOOP
+value=1
+#while value<=10:
+    #print(value)
+    #if value==5:
+        #break
+    #value+=1
+    #print(value)
+    
+#while value<=10:
+ #   value+=1
+  #  if value==5:
+   #     continue
+    #print(value)
+#else:
+ #   print('Value is now equal to '+ str ( value))
+
+
+#FOR LOOPS
+
+names=['Dave','Sara','John']
+#for x in names:
+    #print(x)
+    
+#for x in 'Mississippi':
+    #print(x)
+    
+    
+#for x in names:
+    #if x=='Sara':
+     #   break
+    #print(x)
+    
+#for x in names:
+ #   if x=='Sara':
+  #      continue
+   # print(x)
+   
+#for x in range(4):
+#    print(x)
+
+#for x in range(2,4):
+#    print(x)
+
+#for x in range(-10,101,5):
+#    print(x)
+#else:
+#    print('Glad that\'s over!')
+    
+    
+names=['Dave','Sara','John']
+actions=['codes','eats','sleeps']
+
+#for name in names:
+    #for action in actions:
+        #print(name+' '+action+'.')
+
+#for action in actions:
+ #   for name in names:
+  #      print(name+' '+action+'.')
+  
+  
+#if 'codes' in actions:
+#    print(names)
+#else:
+#    print(actions)
+
+#Go to the rps lesson
