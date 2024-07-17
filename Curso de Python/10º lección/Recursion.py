d2c40460a9a7849539197f4f8d0e06c01534e8c9
Leomtnzz <leomwetzels13@gmail.com>
2024-07-17 17:54:06 +0200

    Add files via upload

---
+++ Curso de Python/10º lección/Recursion.py
@@ -0,0 +1,17 @@
+# def add_one(num):
+#     if (num>=9):
+#         return num+1
+    
+#     total=num+1
+#     print(total)
+
+#     return add_one(total)
+# mynewtotal=add_one(9)
+# print(mynewtotal)
+num=input('Enter your number\n')
+num=int(num)  
+while num<=9:  
+    if num==4:
+        print('Not today buddy')
+    else:print(num+1)
+    break
