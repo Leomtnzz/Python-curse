8eb36e2880253bf5ac8ad77c22077a08f1138968
Leomtnzz <leomwetzels13@gmail.com>
2024-07-27 15:26:44 +0200

    Add files via upload

---
+++ Curso de Python/14º lección/Modules.py
@@ -0,0 +1,25 @@
+import math
+#print(math.pi)
+
+from math import pi
+#print(pi)
+
+import random as rdm
+from rps7 import rock_paper_scissor
+
+import Murcia
+
+
+# for item in dir(rdm):
+#     print(item)
+# x=rdm.choices(3,4)
+# print(x)
+
+#print(Murcia.capital)
+
+#Murcia.randomfunfact()
+
+#print(__name__)
+#print(Murcia.__name__)
+
+rock_paper_scissor()
