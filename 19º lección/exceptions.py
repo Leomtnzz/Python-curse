de1d5f8c4e43e22cbd56b18f643e83d279a3b958
Leomtnzz <leomwetzels13@gmail.com>
2024-08-02 14:16:57 +0200

    Add files via upload
    Here I learnt how to make my own python errors.

---
+++ 19º lección/exceptions.py
@@ -0,0 +1,20 @@
+class loserroresnomolan(Exception):
+    pass
+
+# x=2
+try:
+    raise loserroresnomolan('Esto simplemente no mola.')
+    # print(x/0)
+    # if not type(x) is str:
+    #     raise TypeError('Goldito, pon un str, no int.')
+    # raise Exception('Soy una excepción especial broder. ;)')
+except NameError:
+    print('NameError es que algo no está definido.')
+# except ZeroDivisionError:
+#     print('No puedes dividir entre 0 bro.')
+except ZeroDivisionError as error:
+    print(error)
+else:
+    print('Sin errores.')
+finally:
+    print('Voy a aparecer con o sin errores bro :)')
