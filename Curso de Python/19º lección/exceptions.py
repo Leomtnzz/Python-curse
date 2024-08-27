1c96d73c8a5b9d27616e383eada04aa8c13907f8
leomtnzz <leomwetzels13@gmail.com>
2024-08-27 23:08:52 +0200

    Hecho el primer entorno virtual y usado la primera API

--- Curso de Python/19º lección/exceptions.py
+++ Curso de Python/19º lección/exceptions.py
@@ -1,13 +1,13 @@
 class loserroresnomolan(Exception):
     pass
 
-# x=2
+x=2
 try:
-    raise loserroresnomolan('Esto simplemente no mola.')
-    # print(x/0)
-    # if not type(x) is str:
-    #     raise TypeError('Goldito, pon un str, no int.')
-    # raise Exception('Soy una excepción especial broder. ;)')
+    # raise loserroresnomolan('Esto simplemente no mola.')
+    print(x/0)
+    if not type(x) is str:
+        raise TypeError('Goldito, pon un str, no int.')
+    raise Exception('Soy una excepción especial broder. ;)')
 except NameError:
     print('NameError es que algo no está definido.')
 # except ZeroDivisionError:
